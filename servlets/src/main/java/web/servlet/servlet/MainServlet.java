5242ce1451e83ae1270c4910e20bbe2e5ce424e8
VNK6307 <vnkushchenko@gmail.com>
2024-01-04 20:09:23 +0300

    Variables changed

--- servlets/src/main/java/web/servlet/servlet/MainServlet.java
+++ servlets/src/main/java/web/servlet/servlet/MainServlet.java
@@ -12,6 +12,10 @@ import java.util.regex.Pattern;
 
 public class MainServlet extends HttpServlet {
     private PostController controller;
+    private final static String GET = "GET";
+    private final static String POST = "POST";
+    private final static String DELETE = "DELETE";
+    private final static String targetPath = "/api/posts";
 
     @Override
     public void init() {
@@ -27,20 +31,21 @@ public class MainServlet extends HttpServlet {
             final var method = req.getMethod();
 
             // primitive routing
-            if (method.equals("GET") && path.equals("/api/posts")) {
+            if (method.equals(GET) && path.equals(targetPath)) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+//            if (method.equals(GET) && path.matches("/api/posts/\\d+")) {
+            if (method.equals(GET) && path.matches(targetPath + "/\\d+")) {
                 long id = parseID(path);
                 controller.getById(id, resp);
                 return;
             }
-            if (method.equals("POST") && path.equals("/api/posts")) {
+            if (method.equals(POST) && path.equals(targetPath)) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+            if (method.equals(DELETE) && path.matches(targetPath + "/\\d+")) {
                 long idDel = parseID(path);
                 controller.removeById(idDel, resp);
                 return;
@@ -54,7 +59,7 @@ public class MainServlet extends HttpServlet {
 
     private long parseID(String path) {
         long id;
-        Pattern pattern = Pattern.compile("/api/posts/(\\d+)");
+        Pattern pattern = Pattern.compile(targetPath + "/(\\d+)");
         Matcher matcher = pattern.matcher(path);
 
         if (matcher.matches()) {
